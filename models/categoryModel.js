e61888ba246150026abee051b32a87faf544d1f7
AwadHimself <awadhimself1@gmail.com>
2026-05-14 16:38:09 +0300

    start the ctegory module

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,9 +1,22 @@
 const mongoose = require("mongoose");
 
 //Create schema
-const categorySchema = mongoose.Schema({
-  name: String,
-});
+const categorySchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Category Required"],
+      unique: [true, "Category Must Be Unique"],
+      minlength: [3, "Category Name Is Too Short"],
+      maxlength: [32, "Category Name Is Too long"],
+    },
+    slug: {
+      type: String,
+      tolowercase: true,
+    },
+  },
+  { timestamps: true },
+);
 
 //Create model
 const CategoryModel = mongoose.model("Category", categorySchema);
