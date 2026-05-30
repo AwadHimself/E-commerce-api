25666a131028145bad93f1dc06b72bdf505341fc
awadhimself <awadhimself1@gmail.com>
2026-05-30 23:57:47 +0300

    feat: implement Order model

---
+++ models/orderModel.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+var orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Order Must Belong To A User"],
+    },
+    cartItems: [
+      {
+        product: {
+          type: mongoose.Schema.ObjectId,
+          ref: "Product",
+        },
+        quantity: Number,
+        color: String,
+        price: Number,
+      },
+    ],
+    taxPrice: {
+      type: Number,
+      default: 0,
+    },
+    shippingPrice: {
+      type: Number,
+      default: 0,
+    },
+    totalOrderPrice: {
+      type: Number,
+    },
+    paymentMethodType: {
+      type: String,
+      enum: ["card", "cash"],
+      default: "cash",
+    },
+    ispaid: {
+      type: Boolean,
+      default: false,
+    },
+    paidAt: Date,
+    isDelivered: {
+      type: Boolean,
+      default: false,
+    },
+    deliveredAt: Date,
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Order", orderSchema);
